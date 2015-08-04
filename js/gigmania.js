dc31bb69ca2de7513bebc470daf5f7e9440320ea
gigmania <tyson.daugherty@gmail.com>
2015-08-03 23:39:43 -0700

    added project detail

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,17 +1,30 @@
 
 $( document ).ready(function() {
-
 	$('#yummy-txts').on('mouseup touchend', function () {
-		console.log('stealth clicker working');
 		window.open('http://yummytexts.meteor.com', '_blank');
 	});
 	$('#rollcall').on('mouseup touchend', function () {
-		console.log('stealth clicker working');
 		window.open('http://rollcallin.meteor.com', '_blank');
 	});
 	$('#coderly').on('mouseup touchend', function () {
-		console.log('stealth clicker working');
 		window.open('http://codeup.herokuapp.com', '_blank');
+	}); 
+	$('#oeuvre').on('mouseup touchend', function () {
+		window.open('http://dev.oeuvre.io/', '_blank');
+	}); 
+	$('#artparty').on('mouseup touchend', function () {
+		window.open('http://artparty.meteor.com/', '_blank');
+	}); 
+	$('.launch-festival-vid').on('mouseup touchend', function () {
+		window.open('https://www.youtube.com/watch?v=mCKaYuXA_XE', '_blank');
 	});
-
+	$('#linkedin').on('mouseup touchend', function () {
+		window.open('https://www.linkedin.com/in/tysondaugherty', '_blank');
+	});
+	$('#github').on('mouseup touchend', function () {
+		window.open('https://github.com/gigmania', '_blank');
+	});
+	$('#facebook').on('mouseup touchend', function () {
+		window.open('https://www.facebook.com/tyson.daugherty', '_blank');
+	});
 });
