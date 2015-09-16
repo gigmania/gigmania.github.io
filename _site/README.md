f96e1ab4afeda2e62ccd82e679aa8eaae6374d10
gigmania <tyson.daugherty@gmail.com>
2015-09-16 00:32:59 -0700

    cleaned up tysonium.com

--- _site/README.md
+++ _site/README.md
@@ -1,1 +1,25 @@
-# gigmania.github.io
+TYSONIUM
+========
+###www.tysonium.com
+A personal page highlighting recent projects.
+
+###toyjs
+Javascript calisthenics. Toy problems published weekdays at 9am PT. Toyjs is live at www.toyjs.in and you can find it on Github at https://github.com/gigmania/toyjs.
+
+###Project T
+Project T is a stealth mode communication and media sharing mobile app targeting an underserved, yet high value, customer segment. Development on this project is on hold.
+
+###YummyTexts
+Yummy Texts is a text messaging management platform.
+
+###Roll Call
+Roll Call is a class signup, scheduling and attendance tracking system. Mission Bit used Roll Call to manage students and volunteers during the Spring 2014 semester.
+
+###Coderly
+Coderly is a platform that connects software engineers with volunteer opportunities
+
+###Oeuvre
+I presented Oeuvre at the Launch Festival in February 2014. Oeuvre is a more productised version of Art Party.
+
+###Art Party
+Art Party is a prototype I built at the Launch Festival Hackathon in November 2013. Art Party is the angellist for art.
--- _site/js/gigmania.js
+++ _site/js/gigmania.js
@@ -1,7 +1,10 @@
 
 $( document ).ready(function() {
 	$('#yummy-txts').on('mouseup touchend', function () {
-		window.open('http://yummytexts.meteor.com', '_blank');
+		window.open('https://yummytexts-50519.onmodulus.net/', '_blank');
+	});
+	$('#yummy-txts').on('mouseup touchend', function () {
+		window.open('https://https://yummytexts-50519.onmodulus.net/', '_blank');
 	});
 	$('#rollcall').on('mouseup touchend', function () {
 		window.open('http://rollcallin.meteor.com', '_blank');
@@ -11,9 +14,6 @@ $( document ).ready(function() {
 	}); 
 	$('#oeuvre').on('mouseup touchend', function () {
 		window.open('http://dev.oeuvre.io/', '_blank');
-	}); 
-	$('#artparty').on('mouseup touchend', function () {
-		window.open('http://artparty.meteor.com/', '_blank');
 	}); 
 	$('.launch-festival-vid').on('mouseup touchend', function () {
 		window.open('https://www.youtube.com/watch?v=mCKaYuXA_XE', '_blank');
@@ -29,5 +29,11 @@ $( document ).ready(function() {
 	});
 	$('#twitter').on('mouseup touchend', function () {
 		window.open('https://twitter.com/tysonium', '_blank');
+	});
+	$('.toyjs-repo').on('mouseup touchend', function () {
+		window.open('https://github.com/gigmania/toyjs', '_blank');
+	});
+	$('.toyjs-site').on('mouseup touchend', function () {
+		window.open('http://www.toyjs.in', '_blank');
 	});
 });
