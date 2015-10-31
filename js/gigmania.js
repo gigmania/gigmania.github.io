dcf8b79baf978bf2509127cc611e4aece62aedc1
gigmania <tyson.daugherty@gmail.com>
2015-10-31 08:25:06 -0700

    added gweetux

--- js/gigmania.js
+++ js/gigmania.js
@@ -3,18 +3,30 @@ $( document ).ready(function() {
 	$('#yummy-txts').on('mouseup touchend', function () {
 		window.open('https://yummytexts-50519.onmodulus.net/', '_blank');
 	});
-	$('#yummy-txts').on('mouseup touchend', function () {
-		window.open('https://https://yummytexts-50519.onmodulus.net/', '_blank');
+	$('.yummyT-site').on('mouseup touchend', function () {
+		window.open('https://yummytexts-50519.onmodulus.net/', '_blank');
+	});
+	$('#gweetux').on('mouseup touchend', function () {
+		window.open('https://github.com/gigmania/gweetux', '_blank');
+	});
+	$('.gweetux-site').on('mouseup touchend', function () {
+		window.open('https://github.com/gigmania/gweetux', '_blank');
+	});
+	$('#toyjs').on('mouseup touchend', function () {
+		window.open('http://www.toyjs.in', '_blank');
+	});
+	$('.toyjs-site').on('mouseup touchend', function () {
+		window.open('http://www.toyjs.in', '_blank');
 	});
 	$('#rollcall').on('mouseup touchend', function () {
 		window.open('http://rollcallin.meteor.com', '_blank');
 	});
 	$('#coderly').on('mouseup touchend', function () {
 		window.open('http://codeup.herokuapp.com', '_blank');
-	}); 
+	});
 	$('#oeuvre').on('mouseup touchend', function () {
 		window.open('http://dev.oeuvre.io/', '_blank');
-	}); 
+	});
 	$('.launch-festival-vid').on('mouseup touchend', function () {
 		window.open('https://www.youtube.com/watch?v=mCKaYuXA_XE', '_blank');
 	});
