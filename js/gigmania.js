886c284b57bf936837891a8542526d8bb8dae5eb
gigmania <tyson.daugherty@gmail.com>
2021-05-17 16:46:06 -0700

    hash

--- js/gigmania.js
+++ js/gigmania.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $('.head-name-div').on('mouseup touchend', function() {
     let redirectBase = 'https://app.sandpit.rapidai.com/'
     let hash = window.location.hash;
+    alert(hash);
     let modifiedRedirect = `${redirectBase}${hash}`;
-    alert('--- MODIFIED REDIRECT --- ', modifiedRedirect);
+    alert(modifiedRedirect);
     window.open(modifiedRedirect, '_blank');
   });
   
