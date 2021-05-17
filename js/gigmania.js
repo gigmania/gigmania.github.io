799620435de0feb051ba16d758f3221a360f5615
gigmania <tyson.daugherty@gmail.com>
2021-05-17 16:56:15 -0700

    redirect on load

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  $('.head-name-div').on('mouseup touchend', function() {
+  // $('.head-name-div').on('mouseup touchend', function() {
     let redirectBase = 'http://localhost:8100/'
     let hash = window.location.hash;
     alert(hash);
     let modifiedRedirect = `${redirectBase}${hash}`;
     alert(modifiedRedirect);
     window.open(modifiedRedirect, '_blank');
-  });
+  // });
   
 });
 
