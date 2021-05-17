124f20054767797634bfb1b9a1a98947525efb9d
gigmania <tyson.daugherty@gmail.com>
2021-05-17 16:51:04 -0700

    local host

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('.head-name-div').on('mouseup touchend', function() {
-    let redirectBase = 'https://app.sandpit.rapidai.com/'
+    let redirectBase = 'http://localhost:8100/'
     let hash = window.location.hash;
     alert(hash);
     let modifiedRedirect = `${redirectBase}${hash}`;
