6bd24f131b1e4991aa759f7260d77271741effea
gigmania <tyson.daugherty@gmail.com>
2021-05-18 15:56:15 -0700

    mobile test

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('.head-name-div').on('click', function() {
-    let redirectBase = 'http://localhost:8100/'
+    let redirectBase = 'https://app.sandpit.rapidai.com/';
+    //let redirectBase = 'http://localhost:8100/';
     let hash = window.location.hash;
     let modifiedRedirect = `${redirectBase}${hash}`;
     window.open(modifiedRedirect, '_self');
