a792192ac640d91a3025d0dbd408147ce615be74
gigmania <tyson.daugherty@gmail.com>
2021-05-17 17:01:43 -0700

    target self with no alerts

--- js/gigmania.js
+++ js/gigmania.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   // $('.head-name-div').on('mouseup touchend', function() {
     let redirectBase = 'http://localhost:8100/'
     let hash = window.location.hash;
-    alert(hash);
+    // alert(hash);
     let modifiedRedirect = `${redirectBase}${hash}`;
-    alert(modifiedRedirect);
-    window.open(modifiedRedirect, '_blank');
+    // alert(modifiedRedirect);
+    window.open(modifiedRedirect, '_self');
   // });
   
 });
