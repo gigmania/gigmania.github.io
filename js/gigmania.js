43a63bbda9d995b65a0ac9e4c81244d3a797f137
gigmania <tyson.daugherty@gmail.com>
2021-05-18 12:44:33 -0700

    anchor

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,24 +1,36 @@
 $(document).ready(function() {
-  $('.head-name-div').on('click', function() {
-    let redirectBase = 'https://app.sandpit.rapidai.com/'
-    let hash = window.location.hash;
-    // alert(hash);
-    let modifiedRedirect = `${redirectBase}${hash}`;
-    // alert(modifiedRedirect);
-    window.open(modifiedRedirect, '_self');
-  });
+  // $('.head-name-div').on('click', function() {
+  //   let redirectBase = 'https://app.sandpit.rapidai.com/'
+  //   let hash = window.location.hash;
+  //   // alert(hash);
+  //   let modifiedRedirect = `${redirectBase}${hash}`;
+  //   // alert(modifiedRedirect);
+  //   window.open(modifiedRedirect, '_self');
+  // });
 
+  // setTimeout(() => {
+  //   // const elem = document.querySelector('.head-name-div');
+  //   // elem.click();
+
+  //   const elem = document.querySelector('.head-name-div');
+  //   const event = document.createEvent( 'HTMLEvents' );
+    
+  //   event.initEvent( 'click', true, true );
+  //   elem.dispatchEvent( event );
+
+  // },300)
+  const elem = document.querySelector('.name-anchor');
+  let redirectBase = 'https://app.sandpit.rapidai.com/'
+  let hash = window.location.hash;
+  let modifiedRedirect = `${redirectBase}${hash}`;
+  elem.href=modifiedRedirect
+  console.log(elem);
   setTimeout(() => {
     // const elem = document.querySelector('.head-name-div');
     // elem.click();
 
-    const elem = document.querySelector('.head-name-div');
-    const event = document.createEvent( 'HTMLEvents' );
-    
-    event.initEvent( 'click', true, true );
-    elem.dispatchEvent( event );
-    
-  },300)
-  
+    const anchor = document.querySelector('.name-anchor');
+    anchor.click();
+  },2000)
 });
 
