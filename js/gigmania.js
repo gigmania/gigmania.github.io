d81ff33a3110d0bf859234eca040ea8e68df2c27
gigmania <tyson.daugherty@gmail.com>
2021-05-18 15:39:22 -0700

    blank with local host

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $('.head-name-div').on('click', function() {
-    let redirectBase = 'https://app.sandpit.rapidai.com/'
+    let redirectBase = 'http://localhost:8100/'
     let hash = window.location.hash;
     let modifiedRedirect = `${redirectBase}${hash}`;
-    window.open(modifiedRedirect, '_blank');
+    window.open(modifiedRedirect, '_self');
   });
 });
 
