eab0cfe5ec913f83b19c12e0f4a279a99a00903a
gigmania <tyson.daugherty@gmail.com>
2021-05-18 12:11:43 -0700

    click to open

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  // $('.head-name-div').on('mouseup touchend', function() {
+  $('.head-name-div').on('mouseup touchend', function() {
     let redirectBase = 'https://app.sandpit.rapidai.com/'
     let hash = window.location.hash;
     // alert(hash);
     let modifiedRedirect = `${redirectBase}${hash}`;
     // alert(modifiedRedirect);
     window.open(modifiedRedirect, '_self');
-  // });
+  });
   
 });
 
