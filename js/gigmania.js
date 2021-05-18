71790097e2083405cf3f5c84a5e350a748d818be
gigmania <tyson.daugherty@gmail.com>
2021-05-17 17:24:11 -0700

    sandput app

--- js/gigmania.js
+++ js/gigmania.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // $('.head-name-div').on('mouseup touchend', function() {
-    let redirectBase = 'http://localhost:8100/'
+    let redirectBase = 'https://app.sandpit.rapidai.com/'
     let hash = window.location.hash;
     // alert(hash);
     let modifiedRedirect = `${redirectBase}${hash}`;
